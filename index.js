96ac4b4ef4fc0d44c6bf21b9fefd097cf0f0a45c
Johnson <johnson.nganga@student.moringaschool.com>
2022-09-05 17:44:55 +0300

    completed

---
+++ index.js
@@ -0,0 +1,15 @@
+function receivesAFunction(callback){
+    return callback();
+}
+
+function returnsANamedFunction(){
+    return function NamedFunction(hello){
+        return hello;
+    }
+}
+
+function returnsAnAnonymousFunction(){
+    return function(hello){
+        return hello;
+    }
+}
